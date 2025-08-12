4eaacf6f023e6367f747d2446b1bff52bfd1c3a7
kowshikmeda <168548570+kowshikmeda@users.noreply.github.com>
2025-08-12 22:49:20 +0530

    Time: 96 ms (61.28%), Space: 64.4 MB (7.85%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,20 +1,25 @@
 class Solution {
     public int countPrimes(int n) {
-         if (n <=2) return 0;
-        int []prime=new int[n+1];
-        for(int i=2;i<=n;i++){
-            prime[i]=1;
+        if (n <= 2) return 0; // no primes less than 2
+
+        int[] prime = new int[n]; // size n, since we don't include n
+        for (int i = 2; i < n; i++) {
+            prime[i] = 1;
         }
-        for(int i=2;i*i<=n;i++){
-            if(prime[i]==1){
-                for(int j=i*i;j<=n;j=j+i){
-                    prime[j]=0;
+
+        for (int i = 2; i * i < n; i++) {
+            if (prime[i] == 1) {
+                for (int j = i * i; j < n; j += i) {
+                    prime[j] = 0;
                 }
             }
         }
-        for(int i=2;i<=n;i++){
-            prime[i]=prime[i]+prime[i-1];
+
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            count += prime[i];
         }
-        return prime[n-1];
+
+        return count;
     }
 }
