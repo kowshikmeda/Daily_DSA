842444b2dacf2bb0a78aee91ee721af2230e0f34
kowshikmeda <168548570+kowshikmeda@users.noreply.github.com>
2025-08-13 09:07:40 +0530

    Time: 9 ms (10.87%), Space: 43.7 MB (97.57%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,5 +1,7 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-         return n > 0 && 1162261467 % n == 0;
+        if (n <= 0) return false;
+    double logResult = Math.log10(n) / Math.log10(3);
+    return logResult == Math.floor(logResult);
     }
 }
