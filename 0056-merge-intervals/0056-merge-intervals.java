c886c0a75f92fd883651d57f70728331c466ff1f
kowshikmeda <168548570+kowshikmeda@users.noreply.github.com>
2025-08-22 19:16:44 +0530

    Time: 9 ms (44.34%), Space: 46.7 MB (39.69%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -7,7 +7,7 @@ class Solution {
         Arrays.sort(intervals,(a,b)->a[0]-b[0]);
         int start = intervals[0][0];
         int end = intervals[0][1];
-        for(int i=0;i<intervals.length;i++){
+        for(int i=1;i<intervals.length;i++){
             if(intervals[i][0]<=end){
                 end = Math.max(intervals[i][1],end);
             }else{
