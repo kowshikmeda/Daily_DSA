f556cd4bb26db08e3f0c5410f389a5d3d2dc5f3a
kowshikmeda <168548570+kowshikmeda@users.noreply.github.com>
2025-08-28 12:13:03 +0530

    Time: 0 ms (100%), Space: 42 MB (29.84%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -21,7 +21,7 @@ class Solution {
         if(left==null || right==null ){
             return left==right;
         }
-        if(left.val!=right.val)return false;
-        return helper(left.left,right.right) && helper(left.right,right.left);
+        
+        return (left.val==right.val) &&helper(left.left,right.right) && helper(left.right,right.left);
     }
 }
