cd12b33f80fe257feabe836007dd4df5b0352066
kowshikmeda <168548570+kowshikmeda@users.noreply.github.com>
2025-08-31 09:34:04 +0530

    Time: 0 ms (100%), Space: 45.5 MB (30.71%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.java
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.java
@@ -21,10 +21,11 @@ class Solution {
        if(root.val==val){
         return root;
        }
-       if(root.val<val){
+       else if(root.val<val){
         return searchBST(root.right,val);
-       }
+       }else{
         return searchBST(root.left,val);
+        }
        
     }
 }
